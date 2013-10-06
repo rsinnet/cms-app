2d26a1455a5d342624f19f7b3a75c2af0b429cd5
Ryan W. Sinnet <ryan@rwsinnet.com>
2013-10-05 19:33:11 -0500

    working on SQL statements.

--- .include/image_full.php
+++ .include/image_full.php
@@ -1,8 +1,7 @@
 <span class="image image-full">
-  <img src=".articles/images/<?php echo $foo_image_id; ?>.jpg" alt="" />
-  <p><?php
-    $foo_article_id=1;
-    $con->query("SELECT SELECT * FROM articles_resources_graph WHERE $foo_article_id=1;
+  <img src="images/<?php echo $foo_image_id; ?>.jpg" alt="" />
+  <!--
+    $con->query(SELECT SELECT * FROM articles_resources_graph WHERE $foo_article_id=1;
 
     SELECT * FROM resources WHERE id=(SELECT resource2_id FROM resources_graph WHERE
     resource1_id=(SELECT resource_id FROM articles_resources_graph WHERE article_id=".$foo_article_id."))
@@ -11,5 +10,5 @@
     SELECT * FROM resources WHERE id=(SELECT resource1_id FROM resources_graph WHERE
     resource2_id=(SELECT resource_id FROM articles_resources_graph WHERE article_id=1))
     AND resource_type_id=(SELECT id FROM resource_types WHERE type="Attribution");
-  ?></p>
+  -->
 </span>

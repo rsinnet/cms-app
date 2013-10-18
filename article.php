a265a604f342b6154cf51e1d425804652d4b6ebd
Ryan W. Sinnet <ryan@rwsinnet.com>
2013-10-18 15:50:41 -0500

    Simplifying article.php.

--- article.php
+++ article.php
@@ -27,7 +27,7 @@
   $article_blog_file = "resources/" . $article_id . ".php";
 
   $sql_query = "SELECT title, subtitle, date, topic_id,
-  (SELECT name FROM topics WHERE id='topic_id') AS topic,
+  (SELECT name FROM topics WHERE id=topic_id) AS topic,
   TO_DAYS(NOW()) - TO_DAYS(date) AS age
   FROM articles WHERE id='" . $article_id . "'";
   $result = $con->query($sql_query);
@@ -51,36 +51,16 @@
   -->
 <html>
   <head>
-    <title>
-      <?php echo $article_title; ?> - I AM PHILOSOPHER
-    </title>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,700|Open+Sans+Condensed:700"
-	  rel="stylesheet" />
-    <script src="js/jquery.min.js"></script>
-    <script src="js/config.js"></script>
-    <script src="js/skel.min.js"></script>
-    <script src="js/skel-panels.min.js"></script>
-    <noscript>
-      <link rel="stylesheet" href="css/skel-noscript.css" />
-      <link rel="stylesheet" href="css/style.css" />
-      <link rel="stylesheet" href="css/style-desktop.css" />
-    </noscript>
-    <!--[if lte IE 9]><link rel="stylesheet" href="css/ie9.css" /><![endif]-->
-    <!--[if lte IE 8]><script src="js/html5shiv.js"></script>
-	<link rel="stylesheet" href="css/ie8.css" /><![endif]-->
-    <!--[if lte IE 7]><link rel="stylesheet" href="css/ie7.css" /><![endif]-->
+    <title><?php echo $article_topic_id; ?> - I AM PHILOSOPHER</title>
+    <?php echo file_get_contents('.include/topic_html_head.html'); ?>
   </head>
   <body>
-    
     <!-- Header -->
     <header id="header">
       <div class="logo">
 	<div>
 	  <h1><a href="#" id="logo">I AM PHILOSOPHER</a></h1>
-	  <span class="byline"><?php echo $topic; ?></span>
+	    <span class="byline"><?php echo $topic; ?></span>
 	</div>
       </div>
     </header>
@@ -88,7 +68,6 @@
     
     <!-- Nav -->
     <?php include '.include/navbar.php' ?>
-    <!-- /Nav -->
     
     <!-- Main -->
     <div id="main-wrapper">
@@ -113,115 +92,12 @@
 	    </div>
 	  </div>
 	</div>
-	<div class="row">
-	  <div class="12u">
-	    
-	    <!-- Features -->
-	    <section class="is-features">
-	      <h2 class="major"><span>Valid Commands</span></h2>
-	      <div>
-		<div class="row">
-		  <div class="3u">
-		    
-		    <!-- Feature -->
-		    <section class="is-feature">
-		      <a href="#" class="image image-full"><img src="images/pic01.jpg" alt="" /></a>
-		      <h3><a href="#">Look Up</a></h3>
-		      <p>
-			Phasellus quam turpis, feugiat sit amet ornare in, a hendrerit in 
-			lectus dolore. Praesent semper mod quis eget sed etiam eu ante risus.
-		      </p>
-		    </section>
-		    <!-- /Feature -->
-		    
-		  </div>
-		  <div class="3u">
-		    
-		    <!-- Feature -->
-		    <section class="is-feature">
-		      <a href="#" class="image image-full"><img src="images/pic02.jpg" alt="" /></a>
-		      <h3><a href="#">Look Down</a></h3>
-		      <p>
-			Phasellus quam turpis, feugiat sit amet ornare in, a hendrerit in 
-			lectus dolore. Praesent semper mod quis eget sed etiam eu ante risus.
-		      </p>
-		    </section>
-		    <!-- /Feature -->
-		    
-		  </div>
-		  <div class="3u">
-		    
-		    <!-- Feature -->
-		    <section class="is-feature">
-		      <a href="#" class="image image-full"><img src="images/pic03.jpg" alt="" /></a>
-		      <h3><a href="#">Examine Room</a></h3>
-		      <p>
-			Phasellus quam turpis, feugiat sit amet ornare in, a hendrerit in 
-			lectus dolore. Praesent semper mod quis eget sed etiam eu ante risus.
-		      </p>
-		    </section>
-		    <!-- /Feature -->
-		    
-		  </div>
-		  <div class="3u">
-		    
-		    <!-- Feature -->
-		    <section class="is-feature">
-		      <a href="#" class="image image-full"><img src="images/pic04.jpg" alt="" /></a>
-		      <h3><a href="http://getlamp.com">Get Lamp</a></h3>
-		      <p>
-			Phasellus quam turpis, feugiat sit amet ornare in, a hendrerit in 
-			lectus dolore. Praesent semper mod quis eget sed etiam eu ante risus.
-		      </p>
-		    </section>
-		    <!-- /Feature -->
-		    
-		  </div>
-		</div>
-	      </div>
-	    </section>
-	    <!-- /Features -->
-	    
-	  </div>
-	</div>
       </div>
     </div>
     <!-- /Main -->
     
     <!-- Footer -->
-    <footer id="footer" class="container">
-      <div class="row">
-	<div class="12u">
-	  
-	  <!-- About -->
-	  <section>
-	    <h2 class="major"><span>Under Construction</span></h2>
-	    <p>
-	      This site is currently under construction. The content is fresh
-	      but new features will be added and the layout may change.
-	    </p>
-	  </section>
-	  <!-- /About -->
-	  
-	</div>
-      </div>
-      <div class="row">
-	<div class="12u">
-	  
-	  <!-- Contact -->
-	  <?php echo file_get_contents('.include/footer_contact.html'); ?>
-	  <!-- /Contact -->
-
-	</div>
-      </div>
-      <div class="row">
-	
-	<!-- Copyright -->
-	<?php echo file_get_contents('.include/copyright.html'); ?>
-	
-      </div>
-    </footer>
-    <!-- /Footer -->
+    <?php include '.include/topic_footer.php'; ?>>
     
   </body>
 </html>

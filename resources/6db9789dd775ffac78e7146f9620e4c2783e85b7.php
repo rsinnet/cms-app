37578a608e955cc8e04287832e7c787c669e4809
Ryan W. Sinnet <ryan@rwsinnet.com>
2013-10-18 19:19:44 -0500

    Working on Knowledge Is Forbidden article.

--- resources/6db9789dd775ffac78e7146f9620e4c2783e85b7.php
+++ resources/6db9789dd775ffac78e7146f9620e4c2783e85b7.php
@@ -10,11 +10,11 @@
 
     Using the standard tools &#0150; <a href="http://wikipedia.org/" target="_blank">Wikipedia</a> and <a href="http://scholar.google.com/" target="_blank">Google Scholar</a> &#0150; I was able to glean a cursory understanding of the field including some of the keystone works.
 
-    Naturally, the next step was to scrutinze the more fundamental pieces but alas my journey was to take an unfortunate .
+    Naturally, the next step was to scrutinze the more fundamental pieces but alas my journey was to take an unfortunate turn.
     
     As I attempted to locate numerous articles, I found that access was to be obtained only at great monetary cost to me, the reader, and that not even my large university could provide me with online access.
 
-    This recurring nightmare, this seemingly innocuous impediment for the inquisitive imagination maintains a veil of ignorance upon humanity &#0150; a veil that has been omnipresent even if we may often like to fantasize delusionally about instances of historical utopia.
+    This recurring nightmare, this ostensibly innocuous impediment for the inquisitive imagination maintains a veil of ignorance upon humanity &#0150; a veil that has been omnipresent even if we may often like to fantasize delusionally about instances of historical utopia.
   </p>
   <p>
     The year is 2013 and it is still somehow prohibitively difficult to peruse scholarly work on many important topics.
@@ -24,9 +24,7 @@
     It is understandable and lawful even if unscrupulous that companies maintain trade secrets due the competitive advantages which some such secrets can afford yet the scope is generally limited and the focus esoteric.
 
     But what is more disconcerting by far is the current system of distribution for general scientific literature.
-  </p>
 
-  <p>
     When a scientist publishes his work in a journal or even in the proceedings of a conference, it is often made available online (and sometimes only online) but at a hefty cost to both the writer and the reader.
 
     A fairly new paradigm effected by the Internet modifies the standard model by shifting the cost entirely away from the reader and fully onto the writer.
@@ -45,8 +43,16 @@
 
     O' that the wisdom of ages were not a secret so closely guarded, tucked away for safekeeping to the detriment of all free men, for the quest for knowledge must surely unite all men and women in the bonds of brotherhood.
 
-    And if the people but knew of the ploys of their leaders, they would readily put an end to the intransigent belicosity that seems so prevasive throughout the story of human history and we might finally enjoy peace.
+    And if the people but knew of the ploys of their leaders, they would readily retire to the intransigent belicosity that seems so prevasive throughout the story of human history and we might finally enjoy peace.
 
     In an interview, Julian Assange noted: <q>&#8220;Basically, populations don't like wars, and populations have to be fooled into wars; populations don't willingly, with open eyes, go into a war.&#8221;</q>
+  </p>
+
+  <p>
+    But what are these enigmatic secrets to which I have so cryptically referred which can free a man's mind from the banalties of everyday life?
+
+    Perhaps it is not so much individual secrets but the grander panorama which is increasingly revealed by each piece of knowledge that helps a person transcend.
+
+    Perhaps the typical human is not well trained in logic &#0150; I dare say we can safely accept this as fact &0#150; and his mind is so fazed by emotion and self-assuredness that he cannot reject misinformation and that there is so much misinformation that it sullies his inner canvas which he uses to paint a picture of the world as he perceives it.
   </p>
 </section>
